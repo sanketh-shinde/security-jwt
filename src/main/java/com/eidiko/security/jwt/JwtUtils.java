f3a44fe409e9ec5ffef852c9058218bfb35d8e5a
Sanketh Shinde <sankethshinde99@gmail.com>
2024-06-26 15:35:09 +0530

    initial commit

--- src/main/java/com/eidiko/security/jwt/JwtUtils.java
+++ src/main/java/com/eidiko/security/jwt/JwtUtils.java
@@ -18,6 +18,7 @@ import java.util.function.Function;
 public class JwtUtils {
 
     public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
     public String generateToken(String userName) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userName);
